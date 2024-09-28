28faa5f33af0a62cb04efe92bf485d8c29f797e0
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2024-09-28 11:39:24 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https:',
+                domain: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https:',
+                domain: 'plus.unsplash.com',
+            },
+        ]
+    }
+};
 
 export default nextConfig;
---
+++ src/app/layout/filha/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function PaginaFilhaComLayout() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-5 text-2xl">
+      <h1>Conteudo da Pagina FILHA</h1>
+      <div className="flex gap-5">
+        <Link href="/layout" className="botao">
+          Voltar
+        </Link>
+        <Link href="/" className="botao">
+          Início
+        </Link>
+      </div>
+    </div>
+  );
+}
